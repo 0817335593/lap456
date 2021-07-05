b6347be3a94c729a10f0a2bd45aae9f2658d84c1
0817335593 <85871874+0817335593@users.noreply.github.com>
2021-07-05 14:01:34 +0700

    dsadsad

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -25,6 +25,8 @@ namespace BigSchool.Controllers
             {
                 Categories = _dbContext.Categories.ToList()
             };
+
+
             return View(viewModel);
         }
     }
