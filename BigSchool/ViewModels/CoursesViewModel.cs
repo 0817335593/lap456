9e973acdf0d693f084c7e84e82c90e0a5d8ea876
0817335593 <85871874+0817335593@users.noreply.github.com>
2021-07-12 15:17:30 +0700

    themmmm commit

---
+++ BigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        internal IQueryable<Course> UpcommingCourses;
+
+        public IEnumerable<Course> UpcommingCourse { get; set; } 
+        public bool ShowAction { get; set; }
+    }
+}
